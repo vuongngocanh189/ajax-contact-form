72222301836f0e256acf4d2d9fe186ca5ec861cb
vuongngocanh189 <vuongngocanh189@yahoo.com>
2020-12-10 23:58:04 +0200

    Migrate to Bootstrap 4.0
    + Change well into cards
    + Fix spaces issues
    + Add checkboxes

--- index.php
+++ index.php
@@ -1,30 +1,31 @@
-<?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
-?>
+<?php ?>
 <!DOCTYPE html>
 <head>
   <title>AJAX Contact Form</title>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <style>
-      html {
-    		height: 100%;
-    		min-height: 100%;
-    	}
     	body {
     		background: url('bg.jpg') no-repeat fixed;
-    		min-height: 100%;
     		background-size: cover;
     		-webkit-background-size: cover;
     	  	-moz-background-size: cover;
     	  	-o-background-size: cover;
     	}
-    	form {
-    	  margin-top: 25px;
-    	}
+      .card {
+        margin: 1rem 0;
+        padding: 1rem;
+      }
+      form,
+      article {
+        font-size: 1rem;
+        font-weight: 300;
+      }
+      .form-control:focus {
+        border-color: #17a2b8;
+        box-shadow: none;
+      }
       #submit-btn {
         width: 100%;
       }
@@ -41,85 +42,111 @@
 </head>
 <body>
 <section>
-    <div class="overlay"></div>
-    <div class="container">
-      <div class="col-md-6 col-md-offset-6">
-          <form id="contact" class="form-horizontal well">
-            <div class="form-group">
-              <div class="col-sm-10 col-sm-offset-2">
-                 <h3>Contact Me!</h3>
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="col-md-6 offset-md-6">
+
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title font-weight-bold">Contact Me!</h5>
+          <div class="dropdown-divider"></div>
+
+          <form id="contact" class="form-horizontal mt-4">
+            <div class="form-group row">
+              <label for="name" class="col-sm-3 col-form-label font-weight-bold">Name</label>
+              <div class="col-sm-9">
+                <input type="text" class="form-control" name="name" id="name" placeholder="Your name">
               </div>
             </div>
-            <div class="form-group">
-              <label for="name" class="col-sm-2 control-label">Name</label>
-              <div class="col-sm-10">
-                <input type="name" class="form-control" name="name" id="name" placeholder="Your name">
-              </div>
-            </div>
-            <div class="form-group">
-              <label for="email" class="col-sm-2 control-label">Email</label>
-              <div class="col-sm-10">
+
+            <div class="form-group row">
+              <label for="email" class="col-sm-3 col-form-label font-weight-bold">Email</label>
+              <div class="col-sm-9">
                 <input type="email" class="form-control" name="email" id="email" placeholder="Your email">
               </div>
             </div>
-            <div class="form-group">
-              <label for="email" class="col-sm-2 control-label">Message</label>
-              <div class="col-sm-10">
+
+            <div class="form-group row">
+              <label for="email" class="col-sm-3 col-form-label font-weight-bold">Interest</label>
+              <div class="col-sm-9">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" id="web" value="Web">
+                  <label class="form-check-label" for="web">Web</label>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" id="app" value="App">
+                  <label class="form-check-label" for="app">App</label>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" id="game" value="Game">
+                  <label class="form-check-label" for="game">Game</label>
+                </div>
+              </div>
+            </div>
+
+            <div class="form-group row">
+              <label for="email" class="col-sm-3 col-form-label font-weight-bold">Message</label>
+              <div class="col-sm-9">
                 <textarea rows="10" cols="100" name="message" id="message" class="form-control" style="resize:none" placeholder="Your message"></textarea>
               </div>
             </div>
-              <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                   <div id="msg"></div>
-                   <button class="btn btn-lg btn-info pull-right" id="submit-btn">SEND MESSAGE</button>
-                </div>
+
+            <div class="form-group row">
+              <div class="col-sm-9 offset-sm-3">
+                 <div id="msg"></div>
+                 <button class="btn btn-lg btn-info pull-right" id="submit-btn">SEND MESSAGE</button>
               </div>
+            </div>
           </form>
-        <div class="panel-footer">
-    			<article>
-    				<p>This is a sample website from <a href="https://anhkarppinen.com" target="_blank">Ani's Webdev Blog</a>. Click here for the <a href="https://anhkarppinen.com/ajax-contact-form-without-refreshing/" target="_blank">tutorial and source code</a> of this page.</p>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-body">
+          <article>
+            This is a sample website from <a href="https://anhkarppinen.com" target="_blank">Ani's Webdev Blog</a>. Click here for the <a href="https://anhkarppinen.com/ajax-contact-form-without-refreshing/" target="_blank">tutorial and source code</a> of this page.
           </article>
-	      </div>
+        </div>
       </div>
     </div>
+  </div>
 </section>
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script>
-    /* global $ */
-    $(document).ready(function(){
-      $('#submit-btn').click(function(event){
-        event.preventDefault();
-         $.ajax({
-            dataType: 'JSON',
-            url: 'sendmail.php',
-            type: 'POST',
-            data: $('#contact').serialize(),
-            beforeSend: function(xhr){
-              $('#submit-btn').html('SENDING...');
-            },
-            success: function(response){
-              if(response){
-                console.log(response);
-                if(response['isSuccess']){
-                 $('#msg').html('<div class="alert alert-success">'+ response['msg']  +'</div>');
-                  $('input, textarea').val(function() {
-                     return this.defaultValue;
-                  });
-                }
-                else{
-                  $('#msg').html('<div class="alert alert-danger">'+ response['msg'] +'</div>');
-                }
-              }
-            },
-            error: function(){
-              $('#msg').html('<div class="alert alert-danger">Errors occur. Please try again later.</div>');
-            },
-            complete: function(){
-              $('#submit-btn').html('SEND MESSAGE');
+$(document).ready(function(){
+  $('#submit-btn').click(function(event){
+    event.preventDefault();
+     $.ajax({
+        dataType: 'JSON',
+        url: 'sendmail.php',
+        type: 'POST',
+        data: $('#contact').serialize(),
+        beforeSend: function(xhr){
+          $('#submit-btn').html('SENDING...');
+        },
+        success: function(response){
+          if(response){
+            console.log(response);
+            if(response['isSuccess']){
+             $('#msg').html('<div class="alert alert-success">'+ response['msg']  +'</div>');
+              $('input, textarea').val(function() {
+                 return this.defaultValue;
+              });
             }
-          });
+            else{
+              $('#msg').html('<div class="alert alert-danger">'+ response['msg'] +'</div>');
+            }
+          }
+        },
+        error: function(){
+          $('#msg').html('<div class="alert alert-danger">Errors occur. Please try again later.</div>');
+        },
+        complete: function(){
+          $('#submit-btn').html('SEND MESSAGE');
+        }
       });
-    });
+  });
+});
 </script>
 </html>
